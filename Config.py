870206a773e4bc3f9bbd85215aa43dfe7695197b
Mukhamet1996 <50286432+Mukhamet1996@users.noreply.github.com>
2022-03-23 22:07:52 +0300

    Доработка конфига

--- Config.py
+++ Config.py
@@ -1,11 +1,10 @@
+from operator import index
 import os.path
 check_file = os.path.exists('config.cfg')
 if check_file == False:
     f = open('config.cfg', 'w+')
-
     f.close
-
-    f.write('1' + '1')
+    f.write('1\n' + '1')
     print(f.read(2))
     
 
@@ -13,10 +12,48 @@ elif check_file == True:
 
         f = open('config.cfg', 'r')
         read = f.readlines()
-        
+        print(read)
+        f.close
+
+        f = open('config.cfg', 'r')
+        readline1 = f.readline()
+        f.close
+
         try:
-            print(read[0] + read[1])
+            if read[0] == '1\n':
+                print('Всё ок')
+            elif read[0] == '0\n':
+                print('Всё ок')
+            elif read[0] == '\n':
+                print('Всё ок')
         except:
-            f.close()
             f = open('config.cfg', 'w+')
-            f.write(read[0] + '1')
+            f.write('1')
+            f.close
+            
+        try:
+            f = open('config.cfg', 'r')
+            read = f.readlines()
+            f.close
+            if read[1]!='0' or '1' or '\n':
+                f = open('config.cfg', 'w+')
+                f.write(read[0] + '\n1')
+                f.close
+        except IndexError:
+            f = open('config.cfg', 'w')
+            
+
+        if read[0] == '1\n' or '0\n' or '\n':
+            f = open('config.cfg', 'w+')
+            try:
+                f.write('1\n' + read[1])
+            except IndexError:
+                f.write('1\n1')
+
+            print('Там была белеберда. Я исправиль)')
+            f.close
+
+            f = open('config.cfg', 'r')
+            read = f.readlines()
+            print(read)
+            f.close 
